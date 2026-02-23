3bf7b9a0d41cd20678b30b83a9c86d5d49c27434
TaináDias <tainadias026@gmail.com>
2026-02-23 20:23:13 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once("db.php");
+require_once("globals.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Filmes</title>
+</head>
+<body>
+    <h1>Conexão</h1>
+</body>
+</html>
