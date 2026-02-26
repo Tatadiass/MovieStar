5e95d6f774c80cc7710394242393f5496d495a93
TaináDias <tainadias026@gmail.com>
2026-02-26 20:07:48 -0300

    Add files via upload

--- db.php
+++ db.php
@@ -1,8 +1,10 @@
 <?php
-$db_name = "aulateste";
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = "";
+
+$db_name = "aulateste";   // Nome do banco de dados
+$db_host = "localhost";   // Servidor (no seu caso, o próprio computador)
+$db_user = "root";        // Usuário do MySQL
+$db_pass = "";            // Usuário do MySQL
+
 $conn = new PDO("mysql:dbname=". $db_name .";host=". $db_host, $db_user, $db_pass);
 $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $conn -> setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
--- index.php
+++ index.php
@@ -1,16 +1,5 @@
 <?php
 
-require_once("db.php");
-require_once("globals.php");
+require_once("templates/header.php");       // faz acesso ao arquivo templates/header
+require_once("templates/footer.php");  //faz acesso ao arquivo templates/footer
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Filmes</title>
-</head>
-<body>
-    <h1>Conexão</h1>
-</body>
-</html>
